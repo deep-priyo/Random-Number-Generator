0edd276b502d734286d17afb4a8210ef9d8283ff
deep-priyo <priyadeepmullick@gmail.com>
2024-07-20 21:12:41 +0530

    Describe the changes made to server.py

--- server.py
+++ server.py
@@ -5,9 +5,13 @@ import random
 import datetime
 app = Flask(__name__)
 
-
+@app.route('/')
+def home():
+    random_number = random.randint(1, 10)
+    year=datetime.datetime.now().year
+    return render_template("index.html",random_number=random_number,year=year)
 @app.route("/<int:value>")
-def home(value):
+def fun(value):
     random_number = random.randint(1, value)
     year=datetime.datetime.now().year
     return render_template("index.html",random_number=random_number,year=year)
